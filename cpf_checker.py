dd37bea3d4394ebf452370499ab81f2ade4a2eb2
att4ck3rs3cur1ty <att4ck3rs3cur1ty>
2022-12-30 16:31:44 -0300

    fixing the print for IO error message

--- cpf_checker.py
+++ cpf_checker.py
@@ -38,9 +38,10 @@ class IOchecker:
         '''reads the input CPF list file'''
         try:
             i_file = open(path, mode="r", encoding="utf-8")
+            return i_file
         except Exception as exception:
-            print(Messages.FAILED_OPEN_FILE + exception)
-        return i_file
+            print(Messages.FAILED_OPEN_FILE + str(exception))
+            exit(1)
 
     def output_file(self, path, content):
         '''writes valid CPF's to an external file'''
@@ -50,9 +51,10 @@ class IOchecker:
             o_file = open(path, mode=self.mode, encoding="utf-8")
             o_file.write(content)
             self.first_write = False
+            return o_file
         except Exception as exception:
-            print(Messages.FAILED_OPEN_FILE + exception)
-        return o_file
+            print(Messages.FAILED_OPEN_FILE + str(exception))
+            exit(1)
 
 class FilterCPF:
     '''Call all the methods used to filter CPF'''
